b5903fd87c586148158510a8e79442fef53844ef
everm1nd <everm1nd.mail@gmail.com>
2016-12-11 19:27:19 +0100

    refactored Note class

--- TwoFingersChords/Note.swift
+++ TwoFingersChords/Note.swift
@@ -11,12 +11,11 @@ import Foundation
 class Note {
     private let note_offset = ["C":0, "C#":1, "D":2, "D#":3, "E":4, "F":5, "F#":6, "G":7, "G#":8, "A":9, "A#":10, "B":11]
     
-    var code = ""
+    var code: String // text representation of note+chord
+    var octave: Int
+    var note: String
     
     var toMidiNote: Int {
-        let octave = Int(String(code.characters.last!))! // last character represents octave
-        let note = String(code.characters.dropLast()) // find 1 or 2 characters represent note
-        
         debugPrint(note, octave, (octave * 12 + note_offset[note]!))
         
         return octave * 12 + note_offset[note]!
@@ -24,5 +23,7 @@ class Note {
     
     init(_ code: String) {
         self.code = code
+        self.octave = Int(String(code.characters.last!))! // last character represents octave
+        self.note = String(code.characters.dropLast()) // find 1 or 2 characters represent note
     }
 }
